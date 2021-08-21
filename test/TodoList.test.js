b425049a1c51227dba300243d7d9d4c8a995b43d
stark <aravind.palla@gmail.com>
2021-08-21 17:30:27 +0530

    4. Toggle completed

--- test/TodoList.test.js
+++ test/TodoList.test.js
@@ -19,7 +19,7 @@ contract("TodoList", (accounts) => {
       const taskCount = await this.todoList.taskCount()
       const task = await this.todoList.tasks(taskCount);
       assert.equal(task.id.toNumber(), taskCount.toNumber());
-      assert.equal(task.content, 'Aravinds first TodoList on Ethereum');
+      assert.equal(task.content, "Aravind's first TodoList on Ethereum");
       assert.equal(taskCount.toNumber(), 1)
     })
 
@@ -31,5 +31,14 @@ contract("TodoList", (accounts) => {
       assert.equal(event.id.toNumber(), 2)
       assert.equal(event.content, 'A new task')
       assert.equal(event.completed, false)
+    })
+
+    it('toggles task completion', async () => {
+      const result = await this.todoList.toggleCompleted(1)
+      const task = await this.todoList.tasks(1)
+      assert.equal(task.completed, true)
+      const event = result.logs[0].args
+      assert.equal(event.id.toNumber(), 1)
+      assert.equal(event.completed, true)
     })
 })
